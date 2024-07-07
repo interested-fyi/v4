e1ed7e6e860408cd2f60356f4e8305306f990fd6
hone1er <24376928+hone1er@users.noreply.github.com>
2024-07-06 22:26:21 -0700

    start the company page

--- src/app/explore-talent/page.tsx
+++ src/app/explore-talent/page.tsx
@@ -1,15 +1,12 @@
+import { UserRow } from "./../../components/composed/talent/UserRow";
 import React from "react";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Image from "next/image";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -86,50 +83,7 @@ export default function ExploreTalentPage() {
         </TableHeader>
         <TableBody className='font-body'>
           {users.map((user, index) => (
-            <TableRow key={index} className='py-3 items-center'>
-              <TableCell className='font-medium'>
-                <div className='flex justify-start items-center gap-4'>
-                  <Avatar>
-                    <AvatarImage src='https://randomuser.me/api/port' />
-                    <AvatarFallback>JD</AvatarFallback>
-                  </Avatar>
-                  <div className='flex flex-col gap-1'>
-                    <span>{user.name}</span>
-                    <span className='text-muted-foreground'>{user.role}</span>
-                  </div>
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className='flex justify-center flex-col items-start gap-2'>
-                  <div className='flex items-center gap-2'>
-                    <Image
-                      alt='checkmark'
-                      src='/check.svg'
-                      width={12}
-                      height={12}
-                    />
-                    <p>{user.availability}</p>
-                  </div>
-                  <p>{user.work}</p>
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className='flex md:flex-row flex-col gap-2'>
-                  {user.interests.map((interest, index) => (
-                    <Badge
-                      key={index}
-                      className='gap-2 rounded-sm border border-[#919CF4] bg-[rgba(145,156,244,0.20)] text-[#5063EF] hover:bg-[rgba(145,156,244,.4)] font-body font-medium '
-                    >
-                      <Image src='/amm.svg' alt='amm' height={12} width={12} />{" "}
-                      {interest}
-                    </Badge>
-                  ))}
-                </div>
-              </TableCell>
-              <TableCell className='text-right'>
-                <Button variant={"outline"}>View Profile</Button>
-              </TableCell>
-            </TableRow>
+            <UserRow key={index} index={index} user={user} />
           ))}
         </TableBody>
       </Table>
