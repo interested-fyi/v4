2997cf728344b631caa2830d736686fea721824d
Kyle Banta <kbanta11@gmail.com>
2024-07-12 18:17:36 -0700

    fix main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,9 +48,9 @@ export default function Home() {
               />
             </div>
           </div>
-          <SignupForms />
-          </div>
-        </section>
-      </main>
+        </div>
+        <SignupForms />
+      </section>
+    </main>
   );
 }
