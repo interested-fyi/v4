943fc077c0d503e619377952aeb28de4c767efc8
Kyle Banta <kbanta11@gmail.com>
2024-08-08 16:58:02 -0700

    debug 401 with save-job-postings

--- src/app/api/companies/save-job-postings/route.ts
+++ src/app/api/companies/save-job-postings/route.ts
@@ -7,7 +7,7 @@ import extractJobBody from "@/functions/job-scraping/description_scraper/extract
 import extractJobData from "@/functions/job-scraping/description_scraper/ai-description-scraper";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-    console.log(`save job headers: ${JSON.stringify(req.headers)}`)
+    console.log(`save job headers: ${JSON.stringify(req.headers.get('Authorization'))} / cron sec: ${process.env.CRON_SECRET}`)
     if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
         return NextResponse.json('Unauthorized', { status: 401 });
     }
