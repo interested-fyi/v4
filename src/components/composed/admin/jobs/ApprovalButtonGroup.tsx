75ff9d93616e0f69285649d1e8ed9253af5eb3e7
hone1er <24376928+hone1er@users.noreply.github.com>
2024-08-09 12:11:32 -0700

    add loading states to approve/deny and add revoke functionality

--- src/components/composed/admin/jobs/ApprovalButtonGroup.tsx
+++ src/components/composed/admin/jobs/ApprovalButtonGroup.tsx
@@ -16,41 +16,56 @@ export function ApprovalButtonGroup({
   url,
 }: ApprovalButtonGroupProps) {
   const { getAccessToken } = usePrivy();
+  const [isLoading, setIsLoading] = React.useState(false);
 
-  const handleApproveCompany = async () => {  
-    const accessToken = await getAccessToken();
-    const result = await fetch(`/api/companies/approve`, {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${accessToken}`,
-        "Content-Type": "application",
-      },
-      body: JSON.stringify({
-        companyId: companyId,
-        jobs: jobs,
-        url: url,
-      }),
-    });
-    if (result.ok) {
-      console.log("Company approved");
+  const handleApproveCompany = async () => {
+    setIsLoading(true);
+    try {
+      const accessToken = await getAccessToken();
+      const result = await fetch(`/api/companies/approve`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application",
+        },
+        body: JSON.stringify({
+          companyId: companyId,
+          jobs: jobs,
+          url: url,
+        }),
+      });
+      if (result.ok) {
+        console.log("Company approved");
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
     }
   };
   const handleDenyCompany = async () => {
-    const accessToken = await getAccessToken();
-    const result = await fetch(`/api/companies/deny`, {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${accessToken}`,
-        "Content-Type": "application",
-      },
-      body: JSON.stringify({
-        companyId: companyId,
-        jobs: jobs,
-        approved: false,
-      }),
-    });
-    if (result.ok) {
-      console.log("Company denied");
+    setIsLoading(true);
+    try {
+      const accessToken = await getAccessToken();
+      const result = await fetch(`/api/companies/deny`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application",
+        },
+        body: JSON.stringify({
+          companyId: companyId,
+          jobs: jobs,
+          approved: false,
+        }),
+      });
+      if (result.ok) {
+        console.log("Company denied");
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
@@ -60,6 +75,7 @@ export function ApprovalButtonGroup({
         variant={"outline"}
         className='border-green-700 text-green-700 w-full md:w-[45.5%]'
         onClick={handleApproveCompany}
+        disabled={isLoading}
       >
         Approve
       </Button>
@@ -67,6 +83,7 @@ export function ApprovalButtonGroup({
         variant={"outline"}
         className='border-red-500 text-red-500 w-full md:w-[45.5%]'
         onClick={handleDenyCompany}
+        disabled={isLoading}
       >
         Deny
       </Button>
