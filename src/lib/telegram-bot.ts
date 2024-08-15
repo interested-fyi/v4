275711a1b7abea905ccdda522ac758ebfd0c1e79
Kyle Banta <kbanta11@gmail.com>
2024-08-15 08:05:49 -0700

    more debug testing

--- src/lib/telegram-bot.ts
+++ src/lib/telegram-bot.ts
@@ -5,6 +5,11 @@ const botToken = process.env.NODE_ENV === "development" || process.env.NODE_ENV 
 console.log(`bot token: ${botToken}`)
 const bot = new Bot(botToken);
 
+bot.command('start', async (ctx) => {
+    console.log(ctx.match);
+    await ctx.reply(ctx.match)
+})
+
 bot.on("callback_query:data", async (ctx) => {
     console.log(`Processing referral for ${ctx.callbackQuery.data} from ${JSON.stringify(ctx.callbackQuery.from)}\nMore Context: ${JSON.stringify(ctx.chat)}`);
     const jobId = ctx.callbackQuery.data.replace('job=', '');
@@ -17,7 +22,7 @@ bot.on("callback_query:data", async (ctx) => {
     // await ctx.reply(`Share the below link to share this job\n${telegramPostUrl}`, { parse_mode: 'HTML'});
     await ctx.answerCallbackQuery({
         text: `Join our bot to receive referral links and earn!`,
-        url: 'https://t.me/interested_fyi_dev_bot?start=help'
+        url: `https://t.me/interested_fyi_dev_bot?start=job-${jobId}&tgUrl=${telegramPostUrl}`
     })
     // await ctx.answerCallbackQuery({
     //     text: `Share the below link to share this job\n${telegramPostUrl}`, // generate referral url
