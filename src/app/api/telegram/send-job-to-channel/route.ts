fc6156d5482c022f8ba7c530e5d4ed4a8faae8ba
hone1er <24376928+hone1er@users.noreply.github.com>
2024-08-23 15:55:05 -0700

    update send job to channel with farcaster

--- src/app/api/telegram/send-job-to-channel/route.ts
+++ src/app/api/telegram/send-job-to-channel/route.ts
@@ -4,22 +4,60 @@ import { InlineKeyboard } from "grammy";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-        return NextResponse.json('Unauthorized', { status: 401 });
-    }
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+  const frameURL = "https://api.neynar.com/v2/farcaster/cast";
 
-    try {
-        const { job } = await req.json();
-        console.log(`sending telegram message: ${job.job_posting_id} (${job.posting_url})`);
-        const { message_id } = await sendTelegramMessage(
-            process.env.TELEGRAM_CHANNEL_ID ?? '', 
-            job.summary, 
-            'HTML',
-            new InlineKeyboard().text('Refer a Friend', `job=${job.job_posting_id}`).url('Apply Now', jobUrlBuilder(job.posting_url))
-        );
-        return NextResponse.json({ success: true, url: `https://t.me/${process.env.TELEGRAM_CHANNEL_NAME}/${message_id}`, message_id: message_id }, { status: 200 });
-    } catch (e) {
-        console.error(`Error sending telegram message: ${e}`);
-        return NextResponse.json({ error: e }, { status: 500 });
-    }
+  try {
+    const { job } = await req.json();
+    console.log(
+      `sending telegram message: ${job.job_posting_id} (${job.posting_url})`
+    );
+    const { message_id } = await sendTelegramMessage(
+      process.env.TELEGRAM_CHANNEL_ID ?? "",
+      job.summary,
+      "HTML",
+      new InlineKeyboard()
+        .text("Refer a Friend", `job=${job.job_posting_id}`)
+        .url("Apply Now", jobUrlBuilder(job.posting_url))
+    );
+
+    // send cast containing frame to farcaster
+    const options = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        api_key: process.env.NEYNAR_API_KEY ?? "",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        text: job.summary,
+        embeds: [
+          {
+            url: `${process.env.NEXT_PUBLIC_HOST}/api/farcaster/frames/jobs/${job.job_posting_id}?telegramURL=interestedfyi/33}`,
+          },
+        ],
+      }),
+    };
+
+    fetch(frameURL, options)
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.error("error:" + err));
+
+    return NextResponse.json(
+      {
+        success: true,
+        url: `https://t.me/${process.env.TELEGRAM_CHANNEL_NAME}/${message_id}`,
+        message_id: message_id,
+      },
+      { status: 200 }
+    );
+  } catch (e) {
+    console.error(`Error sending telegram message: ${e}`);
+    return NextResponse.json({ error: e }, { status: 500 });
+  }
 }
