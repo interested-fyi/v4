8d99062c15de2459a00ed9e95be34d9b16fa64fe
hone1er <24376928+hone1er@users.noreply.github.com>
2024-08-27 16:11:49 -0700

    feat: Add UnauthedNav component to AdminDashboard page

--- src/components/composed/Navbar/UnauthedNav.tsx
+++ src/components/composed/Navbar/UnauthedNav.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 interface UnauthedNavProps {
   login: () => void;
 }
 
 const UnauthedNav = ({ login }: UnauthedNavProps) => {
+  const pathname = usePathname();
+
   return (
     <>
       <Link href={"/explore"}>
         <Button variant={"secondary"}>Explore Jobs</Button>
       </Link>
+      {pathname.includes("/admin") && (
+        <Button variant={"secondary"} onClick={login}>
+          Login
+        </Button>
+      )}
     </>
   );
 };
