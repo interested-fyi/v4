f4a6f30dc76337c671a957609d962b5fd0c27e38
hone1er <24376928+hone1er@users.noreply.github.com>
2024-08-29 16:02:40 -0700

    rm unused farcaster logic from telegram route

--- src/app/api/referrals/telegram/route.ts
+++ src/app/api/referrals/telegram/route.ts
@@ -5,45 +5,24 @@ export async function POST(req: Request) {
   const { userId, fid, jobId, telegramPostUrl } = await req.json();
 
   try {
-    if (userId) {
-      // save link click to supabase
-      console.log(
-        `Link clicked: user ${userId}, job ${jobId} - ${telegramPostUrl}`
-      );
+    // save link click to supabase
+    console.log(
+      `Link clicked: user ${userId}, job ${jobId} - ${telegramPostUrl}`
+    );
 
-      // log click in referral_link_clicks table for user, source and jobId
-      const { error } = await supabase.from("referral_link_click").insert({
-        job_id: jobId,
-        telegram_user_id: userId,
-        url: telegramPostUrl,
-        source: "telegram",
-      });
+    // log click in referral_link_clicks table for user, source and jobId
+    const { error } = await supabase.from("referral_link_click").insert({
+      job_id: jobId,
+      telegram_user_id: userId,
+      url: telegramPostUrl,
+      source: "telegram",
+    });
 
-      if (error) {
-        throw new Error("Error logging link click");
-      }
-
-      return NextResponse.json({ success: true });
-    } else if (fid) {
-      // save link click to supabase
-      console.log(
-        `Link clicked: fid ${fid}, job ${jobId} - ${telegramPostUrl}`
-      );
-
-      // log click in referral_link_clicks table for user, source and jobId
-      // const { error } = await supabase.from("referral_link_click").insert({
-      //   job_id: jobId,
-      //   farcaster_user_id: fid,
-      //   url: telegramPostUrl,
-      //   source: "farcaster",
-      // });
-
-      // if (error) {
-      //   throw new Error("Error logging link click");
-      // }
-
-      return NextResponse.json({ success: true });
+    if (error) {
+      throw new Error("Error logging link click");
     }
+
+    return NextResponse.json({ success: true });
   } catch (error) {
     console.error("Error logging referral:", error);
     return NextResponse.json({ success: false, error: error }, { status: 500 });
