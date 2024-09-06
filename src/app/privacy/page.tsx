09157efd8360baa116bf3c3fc4174687f241b72e
hone1er <24376928+hone1er@users.noreply.github.com>
2024-09-05 19:39:13 -0700

    add privacy and tos

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,162 @@
+import React from "react";
+
+const PrivacyPage = () => {
+  return (
+    <div>
+      <h1>INTERESTED PRIVACY POLICY</h1>
+      <p>
+        Interested (the “Company”) is committed to maintaining robust privacy
+        protections for its users. Our Privacy Policy (“Privacy Policy”) is
+        designed to help you understand how we collect, use, and safeguard the
+        information you provide to us and to assist you in making informed
+        decisions when using our Service.
+      </p>
+      <p>
+        For purposes of this Agreement, “Site” refers to the Company’s website,
+        which can be accessed at{" "}
+        <a href='https://interested.fyi'>https://interested.fyi</a>.
+      </p>
+      <p>
+        “Service” refers to the Company’s services accessed via the Site, in
+        which users can [describe the services provided by Interested].
+      </p>
+      <p>The terms “we,” “us,” and “our” refer to the Company.</p>
+      <p>“You” refers to you, as a user of our Site or our Service.</p>
+      <p>
+        By accessing our Site or our Service, you accept our Privacy Policy and
+        Terms of Use (found here: [insert link to Terms of Use]), and you
+        consent to our collection, storage, use, and disclosure of your Personal
+        Information as described in this Privacy Policy.
+      </p>
+      <h2>I. INFORMATION WE COLLECT</h2>
+      <p>
+        We collect “Non-Personal Information” and “Personal Information.”
+        Non-Personal Information includes information that cannot be used to
+        personally identify you, such as anonymous usage data, general
+        demographic information we may collect, referring/exit pages and URLs,
+        platform types, preferences you submit and preferences that are
+        generated based on the data you submit and number of clicks. Personal
+        Information includes your email [and other personal information
+        collected by Interested], which you submit to us through the
+        registration process at the Site.
+      </p>
+      <h2>II. HOW WE USE AND SHARE INFORMATION</h2>
+      <p>
+        Personal Information:
+        <br />
+        Except as otherwise stated in this Privacy Policy, we do not sell,
+        trade, rent, or otherwise share for marketing purposes your Personal
+        Information with third parties without your consent. We do share
+        Personal Information with vendors who are performing services for the
+        Company, such as the servers for our email communications who are
+        provided access to user’s email address for purposes of sending emails
+        from us. Those vendors use your Personal Information only at our
+        direction and in accordance with our Privacy Policy.
+      </p>
+      <p>
+        In general, the Personal Information you provide to us is used to help
+        us communicate with you. For example, we use Personal Information to
+        contact users in response to questions, solicit feedback from users,
+        provide technical support, and inform users about promotional offers.
+      </p>
+      <p>
+        We may share Personal Information with outside parties if we have a
+        good-faith belief that access, use, preservation, or disclosure of the
+        information is reasonably necessary to meet any applicable legal process
+        or enforceable governmental request; to enforce applicable Terms of
+        Service, including investigation of potential violations; address fraud,
+        security, or technical concerns; or to protect against harm to the
+        rights, property, or safety of our users or the public as required or
+        permitted by law.
+      </p>
+      <p>
+        Non-Personal Information:
+        <br />
+        In general, we use Non-Personal Information to help us improve the
+        Service and customize the user experience. We also aggregate
+        Non-Personal Information in order to track trends and analyze use
+        patterns on the Site. This Privacy Policy does not limit in any way our
+        use or disclosure of Non-Personal Information and we reserve the right
+        to use and disclose such Non-Personal Information to our partners,
+        advertisers, and other third parties at our discretion.
+      </p>
+      <p>
+        In the event we undergo a business transaction such as a merger,
+        acquisition by another company, or sale of all or a portion of our
+        assets, your Personal Information may be among the assets transferred.
+        You acknowledge and consent that such transfers may occur and are
+        permitted by this Privacy Policy, and that any acquirer of our assets
+        may continue to process your Personal Information as set forth in this
+        Privacy Policy. If our information practices change at any time in the
+        future, we will post the policy changes to the Site so that you may opt
+        out of the new information practices. We suggest that you check the Site
+        periodically if you are concerned about how your information is used.
+      </p>
+      <h2>III. HOW WE PROTECT INFORMATION</h2>
+      <p>
+        We implement security measures designed to protect your information from
+        unauthorized access. Your account is protected by your account password
+        and we urge you to take steps to keep your personal information safe by
+        not disclosing your password and by logging out of your account after
+        each use. We further protect your information from potential security
+        breaches by implementing certain technological security measures
+        including encryption, firewalls, and secure socket layer technology.
+        However, these measures do not guarantee that your information will not
+        be accessed, disclosed, altered, or destroyed by breach of such
+        firewalls and secure server software. By using our Service, you
+        acknowledge that you understand and agree to assume these risks.
+      </p>
+      <h2>IV. YOUR RIGHTS REGARDING THE USE OF YOUR PERSONAL INFORMATION</h2>
+      <p>
+        You have the right at any time to prevent us from contacting you for
+        marketing purposes. When we send a promotional communication to a user,
+        the user can opt out of further promotional communications by following
+        the unsubscribe instructions provided in each promotional e-mail. You
+        can also indicate that you do not wish to receive marketing
+        communications from us in the [list location of opt-out page, i.e.
+        “Settings” section] of the Site. Please note that notwithstanding the
+        promotional preferences you indicate by either unsubscribing or opting
+        out in the [location of opt-out page] of the Site, we may continue to
+        send you administrative emails including, for example, periodic updates
+        to our Privacy Policy.
+      </p>
+      <h2>V. LINKS TO OTHER WEBSITES</h2>
+      <p>
+        As part of the Service, we may provide links to or compatibility with
+        other websites or applications. However, we are not responsible for the
+        privacy practices employed by those websites or the information or
+        content they contain. This Privacy Policy applies solely to information
+        collected by us through the Site and the Service. Therefore, this
+        Privacy Policy does not apply to your use of a third party website
+        accessed by selecting a link on our Site or via our Service. To the
+        extent that you access or use the Service through or on another website
+        or application, then the privacy policy of that other website or
+        application will apply to your access or use of that site or
+        application. We encourage our users to read the privacy statements of
+        other websites before proceeding to use them.
+      </p>
+      <h2>VI. CHANGES TO OUR PRIVACY POLICY</h2>
+      <p>
+        The Company reserves the right to change this policy and our Terms of
+        Service at any time. We will notify you of significant changes to our
+        Privacy Policy by sending a notice to the primary email address
+        specified in your account or by placing a prominent notice on our site.
+        Significant changes will go into effect 30 days following such
+        notification. Non-material changes or clarifications will take effect
+        immediately. You should periodically check the Site and this privacy
+        page for updates.
+      </p>
+      <h2>VII. CONTACT US</h2>
+      <p>
+        If you have any questions regarding this Privacy Policy or the practices
+        of this Site, please contact us by sending an email to{" "}
+        <a href='mailto:contact@interested.fyi'>contact@interested.fyi</a>.
+      </p>
+      <p>
+        Last Updated: This Privacy Policy was last updated on December 19, 2023.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPage;
