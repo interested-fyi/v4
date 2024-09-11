f692af79f5fcad00c2e7d7538e97d3e5f75b03d2
hone1er <24376928+hone1er@users.noreply.github.com>
2024-09-11 11:20:05 -0700

    rm image on landing for medium screen sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className='flex  min-h-screen flex-col gap-0 items-center justify-start '>
       <section className='w-full max-w-full bg-[#2640EB] py-24 sm:p-8 p-2 md:p-24'>
-        <div className='flex relative md:flex-row flex-col w-full'>
+        <div className='flex relative lg:flex-row flex-col w-full'>
           <div className='flex flex-col gap-8 relative z-10'>
             <div className='flex flex-col text-center gap-6 md:gap-4 max-w-[303px] m-auto'>
               <h1 className='font-heading text-4xl md:text-6xl font-bold text-[#919CF4] '>
@@ -32,7 +32,7 @@ export default function Home() {
             </p> */}
           </div>
 
-          <div className='absolute right-0 md:right-10 top-0 h-full w-[50vw] z-[0] hidden md:flex place-items-center'>
+          <div className='absolute right-0 md:right-10 top-0 h-full w-[50vw] z-[0] hidden lg:flex place-items-center'>
             <div className='relative h-full w-full'>
               <Image
                 className='dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
