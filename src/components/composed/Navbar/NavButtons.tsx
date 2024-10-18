a59a65988210ca4583f74efed87042df5436f2af
hone1er <24376928+hone1er@users.noreply.github.com>
2024-10-17 17:01:53 -0700

    add explore talent button

--- src/components/composed/Navbar/NavButtons.tsx
+++ src/components/composed/Navbar/NavButtons.tsx
@@ -6,6 +6,12 @@ export function NavButtons() {
   return (
     <div className='flex md:flex-row flex-row gap-4 '>
       <div className='flex gap-4'>
+        {" "}
+        <Link href={"/explore-talent"}>
+          <Button className='w-16 h-8 px-4 text-xs text-black bg-[#919cf4] bg-opacity-30 hover:bg-opacity-90 hover:bg-[#919cf4] md:text-sm '>
+            Talent
+          </Button>
+        </Link>
         <Link href={"https://t.me/interestedfyi"} target='_blank'>
           <Button className='w-10 h-8 p-0 text-xs md:text-sm bg-[#919cf4] bg-opacity-30 hover:bg-opacity-90 hover:bg-[#919cf4]'>
             <Image
