6178e177a3563092c0fcccd9813f147c1e9a089e
hone1er <24376928+hone1er@users.noreply.github.com>
2024-11-20 12:28:42 -0800

    fix no activity

--- src/components/composed/profile/ActivityTab.tsx
+++ src/components/composed/profile/ActivityTab.tsx
@@ -70,7 +70,7 @@ export default function ActivityTab({
     );
   }
 
-  if (warpCastError) {
+  if (!warpcastActivity && !GitHubActivityCard && !isLoading) {
     return <EmptyActivityFeedComponent />;
   }
 
