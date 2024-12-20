01d3d133dde59df958e548bc79debeff2c854b18
hone1er <24376928+hone1er@users.noreply.github.com>
2024-12-20 10:45:01 -0800

    add revoke cron job

---
+++ src/app/api/cron/companies/run-revoke-onchain-attestation/route.ts
@@ -0,0 +1,150 @@
+import { NextRequest, NextResponse } from "next/server";
+import easAbi from "@ethereum-attestation-service/eas-contracts/deployments/optimism/EAS.json";
+import { mnemonicToAccount } from "viem/accounts";
+import { createWalletClient, http } from "viem";
+import { optimism, optimismSepolia } from "viem/chains";
+import { publicClient } from "@/lib/viemClient";
+import supabase from "@/lib/supabase";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  console.log("Incoming request to scrape job details...");
+
+  if (
+    req.headers.get("Authorization") === `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    console.log("Unauthorized request. Authorization header does not match.");
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+
+  // Step 1: Fetch job_posting_ids from job_details_last_scraping where active = false
+  const { data: jobDetails, error: jobDetailsError } = await supabase
+    .from("job_details_last_scraping")
+    .select("id")
+    .eq("active", false)
+    .limit(5);
+
+  if (jobDetailsError) throw jobDetailsError;
+
+  const jobDetailsIDs = jobDetails.map((detail) => detail.id);
+  console.log("🚀 ~ GET ~ jobDetailsIDs:", jobDetailsIDs);
+
+  // Step 2: Fetch job_posting_ids from job_postings_details matching jobDetailsIDs
+  const { data: jobPostings, error: jobPostingsError } = await supabase
+    .from("job_postings_details")
+    .select("job_posting_id")
+    .in("job_posting_id", jobDetailsIDs);
+  console.log("🚀 ~ GET ~ jobPostings:", jobPostings);
+
+  if (jobPostingsError) throw jobPostingsError;
+
+  const jobPostingIDs = jobPostings.map((posting) => posting.job_posting_id);
+  console.log("🚀 ~ GET ~ jobPostingIDs:", jobPostingIDs);
+
+  // Step 3: Fetch attestation_uids from job_attestations where active = true and job_posting_id matches
+  const { data: attestations, error: attestationsError } = await supabase
+    .from("job_attestations")
+    .select("attestation_uid")
+    .eq("active", true)
+    .in("job_posting_id", jobPostingIDs);
+  console.log("🚀 ~ GET ~ attestations:", attestations);
+
+  if (attestationsError) throw attestationsError;
+
+  const account = mnemonicToAccount(process.env.ADMIN_MNEMONIC as string);
+  const client = createWalletClient({
+    account,
+    chain:
+      process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
+        ? optimismSepolia
+        : optimism,
+    transport: http(),
+  });
+  // Attestation creation
+  const contractParams = {
+    address: process.env.NEXT_PUBLIC_EAS_CONTRACT_ADDRESS as `0x${string}`,
+    abi: easAbi.abi,
+    functionName: "multiRevoke",
+    args: [
+      [
+        {
+          schema:
+            "0xc67b74fbed99776f3949844924f1a7f2641dfe34e39707b3bb97684c711b1d43",
+          data: attestations.map((attestation) => ({
+            uid: attestation.attestation_uid,
+            value: 0,
+          })),
+        },
+      ],
+    ],
+    chain:
+      process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
+        ? optimismSepolia
+        : optimism,
+    account: client.account,
+  };
+
+  console.log(
+    "🚀 ~ GET ~ attestations.map:",
+    attestations.map((attestation) => ({
+      uid: attestation.attestation_uid,
+      value: 0,
+    }))
+  );
+  console.log("Checking account balance...");
+  const balance = await publicClient.getBalance({
+    address: client.account.address,
+  });
+
+  console.log("Account balance:", balance.toString());
+  if (BigInt(balance) === BigInt(0)) {
+    throw new Error(
+      "Account has insufficient funds to perform this transaction."
+    );
+  }
+  if (attestations.length === 0) {
+    console.log("No attestations to revoke.");
+    return NextResponse.json("No attestations to revoke", { status: 200 });
+  }
+
+  try {
+    console.log("Simulating attestation contract call...");
+    const nonce =
+      (await publicClient.getTransactionCount({
+        address: account.address,
+      })) + 3;
+
+    const { request } = await publicClient.simulateContract({
+      ...contractParams,
+      nonce,
+      account: client.account,
+    });
+
+    console.log("🚀 ~ GET ~ request:", request);
+
+    const txHash = await client.writeContract({
+      ...request,
+      account: client.account,
+      nonce,
+    });
+
+    console.log("Attestation Transaction Hash:", txHash);
+    if (txHash) {
+      //update job_attestations set active = false where attestation_uid in attestations
+      const { error: updateAttestationsError } = await supabase
+        .from("job_attestations")
+        .update({ active: false })
+        .in(
+          "attestation_uid",
+          attestations.map((attestation) => attestation.attestation_uid)
+        );
+
+      if (updateAttestationsError) {
+        throw Error("Error updating job_attestations");
+      }
+    }
+    return NextResponse.json("Success", { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json("Error", { status: 500 });
+  }
+}
