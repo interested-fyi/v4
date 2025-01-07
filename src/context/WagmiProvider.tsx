55bb32562d9e637302e2992df7676f2a58f65139
hone1er <24376928+hone1er@users.noreply.github.com>
2025-01-07 14:43:08 -0800

    fix: prevent server-side rendering for Web3Provider component

--- src/context/WagmiProvider.tsx
+++ src/context/WagmiProvider.tsx
@@ -2,32 +2,39 @@
 import React from "react";
 import { type State, WagmiProvider, createConfig, http } from "wagmi";
 import { mainnet, optimism } from "wagmi/chains";
-
-import { type PropsWithChildren } from "react";
+import { useEffect, useState, type PropsWithChildren } from "react";
 import { coinbaseWallet, injected, metaMask, safe } from "wagmi/connectors";
 
 interface Props extends PropsWithChildren {
   initialState?: State;
 }
 
-const config = createConfig({
-  chains: [mainnet, optimism],
-  connectors: [
-    coinbaseWallet({ appName: "Interested.FYI" }),
-    injected(),
-    metaMask(),
-    safe(),
-  ],
-  ssr: true,
-  transports: {
-    [optimism.id]: http(),
-    [mainnet.id]: http(),
-  },
-});
-
-// 3. Create moda
-
 export function Web3Provider({ initialState, children }: Props) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const config = createConfig({
+    chains: [mainnet, optimism],
+    connectors: [
+      coinbaseWallet({ appName: "Interested.FYI" }),
+      injected(),
+      metaMask(),
+      safe(),
+    ],
+    ssr: true,
+    transports: {
+      [optimism.id]: http(),
+      [mainnet.id]: http(),
+    },
+  });
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <WagmiProvider initialState={initialState} config={config}>
       {children}
