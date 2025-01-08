8139d8d2f7ccc5f4b60abcd7c764ebefaf80786e
hone1er <24376928+hone1er@users.noreply.github.com>
2025-01-08 12:05:28 -0800

    fix: update import path for globals.css in company details layout

--- src/app/company-details/[company-id]/layout.tsx
+++ src/app/company-details/[company-id]/layout.tsx
@@ -1,4 +1,4 @@
-import "./../../../../globals.css";
+import "./../../globals.css";
 import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
