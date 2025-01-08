765c0dea6255dc8554d8b0b9daeb5eed39815586
hone1er <24376928+hone1er@users.noreply.github.com>
2025-01-08 11:29:24 -0800

    fix: enable client-side rendering for ServerExploreTalentPage component

--- src/app/company-details/[company-id]/page.tsx
+++ src/app/company-details/[company-id]/page.tsx
@@ -2,9 +2,18 @@
 
 import CompanyInfo from "@/components/composed/companies/CompanyInfo";
 import JobTable from "@/components/composed/jobs/JobTable";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ServerExploreTalentPage = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
   return (
     <div className='flex flex-col gap-8'>
       <CompanyInfo />
